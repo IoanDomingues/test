5033fba7fbb4745d8f50140f0433f5fa7f4b489f
Ioan Domingues <ioandomingues944500@gmail.com>
2025-10-20 16:25:13 +0200

    test4

--- ci_demo/tests/test-example.py
+++ ci_demo/tests/test-example.py
@@ -1,13 +1,10 @@
 # ci_demo/tests/test_sample.py
 import unittest
-from fichier import addition, multiplication
+from fichier import addition
 
-class TestCalculs(unittest.TestCase):
-
-    def test_addition(self):
+class TestAddition(unittest.TestCase):
+    def test_add(self):
         self.assertEqual(addition(2, 3), 5)
-        self.assertEqual(addition(-1, 1), 0)
-
 
 if __name__ == '__main__':
     unittest.main()
