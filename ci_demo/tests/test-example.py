d14cd89b8b885f742dec9e7c3cb90ffb8e7e80ac
Ioan Domingues <ioandomingues944500@gmail.com>
2025-10-20 16:13:59 +0200

    test

--- ci_demo/tests/test-example.py
+++ ci_demo/tests/test-example.py
@@ -1,9 +1,16 @@
-def addition(a, b):
-    return a + b
+# ci_demo/tests/test_sample.py
+import unittest
+from fichier import addition, multiplication
 
-def soustraction(a, b):
-    return a - b
+class TestCalculs(unittest.TestCase):
 
-if __name__ == "__main__":
-    print("Addition 5 + 3 =", addition(5, 3))
-    print("Soustraction 5 - 3 =", soustraction(5, 3))
+    def test_addition(self):
+        self.assertEqual(addition(2, 3), 5)
+        self.assertEqual(addition(-1, 1), 0)
+
+    def test_multiplication(self):
+        self.assertEqual(multiplication(3, 4), 12)
+        self.assertEqual(multiplication(-2, 3), -6)
+
+if __name__ == '__main__':
+    unittest.main()
