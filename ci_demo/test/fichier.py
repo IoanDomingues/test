971172e0b66aec86bebb92d5deeceab0e28dabf6
Ioan Domingues <ioandomingues944500@gmail.com>
2025-10-20 16:01:43 +0200

    testttttt

---
+++ ci_demo/test/fichier.py
@@ -0,0 +1,9 @@
+def addition(a, b):
+    return a + b
+
+def soustraction(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print("Addition 5 + 3 =", addition(5, 3))
+    print("Soustraction 5 - 3 =", soustraction(5, 3))
